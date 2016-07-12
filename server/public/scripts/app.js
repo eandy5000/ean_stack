ca37d9a81dcb25815b5dfda75df66bc30a152966
E. Andrew Eccleston <eandreweccleston@Es-MacBook-Pro.local>
2016-07-12 09:35:00 -0500

    angular basic routing works

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -9,7 +9,28 @@ console.log('working');
 
 }]);
 
+app.config(['$routeProvider',function($routeProvider){
+    $routeProvider
+        .when('/', {
+            templateUrl: './pages/home.html'
+        })
+        .when('/movies', {
+            templateUrl: './pages/movies.html'
+        })
+        .when('/species',{
+            templateUrl: './pages/species.html'
+        })
+        .when('/chum',{
+            templateUrl: './pages/chum.html'
+        })
+        .when('/404',{
+            templateUrl: './pages/404.html'
+        })
+        .otherwise({
+            redirectTo : '/404'
+        });
 
+}])
 
 
 })();
